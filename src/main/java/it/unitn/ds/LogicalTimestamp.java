886dc06547811b47313aaa8de8ecc6e614fa18e1
Francesco Marchetto <francesco.marchett-2@studenti.unitn.it>
2026-09-25 20:04:59 +0200

    Implemented equals and hashCode

--- src/main/java/it/unitn/ds/LogicalTimestamp.java
+++ src/main/java/it/unitn/ds/LogicalTimestamp.java
@@ -1,6 +1,7 @@
 package it.unitn.ds;
 import java.io.Serializable;
 import java.lang.Comparable;
+import java.util.Objects;
 
 public class LogicalTimestamp implements Serializable, Comparable<LogicalTimestamp> {
   public final int epoch;
@@ -18,5 +19,23 @@ public class LogicalTimestamp implements Serializable, Comparable<LogicalTimesta
       return this.sequence_number - rhs.sequence_number;
     else
       return epoch_comparison;
+  }
+
+  @Override
+  public boolean equals(Object rhs) {
+    // These ifs are not very efficient and it would be better to
+    // cramp everything into a single boolean expression,
+    // but it would be unreadable
+    if(this == rhs)
+      return true;
+    if(rhs == null || !(rhs instanceof LogicalTimestamp))
+      return false;
+    LogicalTimestamp rhs_timestamp = (LogicalTimestamp) rhs;
+    return this.epoch == rhs_timestamp.epoch && this.sequence_number == rhs_timestamp.sequence_number;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(epoch, sequence_number);
   }
 }
